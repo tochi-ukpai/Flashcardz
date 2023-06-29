35cc883cc0fb1f307312f80f627895230c01bd65
Tochukwu Ukpai <tochukwuukpai@gmail.com>
2023-06-30 00:43:11 +0100

    Initial Commit

---
+++ Flashcardz/FlashcardzApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardzApp.swift
+//  Flashcardz
+//
+//  Created by Theós on 30/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlashcardzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
